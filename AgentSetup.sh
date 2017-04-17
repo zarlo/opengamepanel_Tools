25be12c4440582f655ca6828c271e4d552012109
zarlo <zarlo5899@gmail.com>
2017-04-17 19:51:05 +1000

    Update AgentSetup.sh

--- AgentSetup.sh
+++ AgentSetup.sh
@@ -6,7 +6,7 @@ sudo apt-get install libxml-parser-perl libpath-class-perl perl-modules screen r
 sudo apt-get install libc6-i386 libgcc1:i386
 sudo apt-get install lib32gcc1
 sudo apt-get install libhttp-daemon-perl
-sudo apt-get install mailutils util-linux tmux lib32gcc1 libstdc++6 libstdc++6:i386 libcurl4-gnutls-dev:i386
+sudo apt-get lib32gcc1 libstdc++6 libstdc++6:i386 libcurl4-gnutls-dev:i386
 wget -N "https://github.com/OpenGamePanel/Easy-Installers/raw/master/Linux/Debian-Ubuntu/ogp-agent-latest.deb" -O "ogp-agent-latest.deb"
 sudo dpkg -i "ogp-agent-latest.deb"
 sudo cat /root/ogp_user_password
